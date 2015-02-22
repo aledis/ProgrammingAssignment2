e464a43ab59db2f70789263eadf3373a561c8580
Sasha <sasha@Alexanders-MBP.home>
2015-02-22 11:34:45 -0500

    Solution to assignment 2 for the R programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a closure over the matrix passed in 
+## as a parameter with methods to access that matrix and 
+## to store/retrieve inverse of that matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix() ) {
+        inverse <- NULL
+        
+        ## closure over matrix
+        getMatrix <- function() matrix
+        
+        ## cache inverse
+        setInverse <- function(inverseMatrix) inverse <<- inverseMatrix 
+        ## get cached inverse
+        getInverse <- function() inverse
+        
+        list(getMatrix = getMatrix, setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve retrieves the inverse of the matrix from cache 
+## if it is available, otherwise inverse matrix is calculated
+## and stored in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get cached inverse
+        inverseMatrix <- x$getInverse()
+        if(!is.null(inverseMatrix)) {
+                ## cached inverse is not null, so return it
+                return(inverseMatrix)
+        }
+        ## cached inverse was null, get matrix itself and
+        ## calculate the inverseinverse
+        matrix <- x$getMatrix()
+        inverseMatrix <- solve(matrix)
+        x$setInverse(inverseMatrix)
+        inverseMatrix
 }
